705a1578ee6c35e144f2701509b4044fc25f27b6
Ellen <ellen.stabel@student.kuleuven.be>
2021-10-22 14:56:46 +0200

    schermen

--- App/App_functies.py
+++ App/App_functies.py
@@ -1,7 +1,38 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class TitleScreen(Screen):
+    pass
+
+
+class MainScreen(Screen):
+    pass
+
+
+class ECGScreen(Screen):
+    pass
+
+
+class PPGScreen(Screen):
+    pass
+
+
+class EDAScreen(Screen):
+    pass
 
 
 class FilterApp(App):
-    pass
+    def build(self):
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(TitleScreen(name='title'))
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(ECGScreen(name='ECG'))
+        sm.add_widget(PPGScreen(name='PPG'))
+        sm.add_widget(EDAScreen(name='EDA'))
+        return sm
+
 
 FilterApp().run()
