23f49101ce07b2f66f1e3a326b23ffc441329eea
Ignace Van Nederkassel <ignace.vannederkassel@student.kuleuven.be>
2021-10-22 15:43:59 +0200

    4

--- App/App_functies.py
+++ App/App_functies.py
@@ -2,8 +2,12 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
-"""
+
+
+
 class TitleScreen(Screen):
     pass
 
@@ -22,14 +26,17 @@ class PPGScreen(Screen):
 
 class EDAScreen(Screen):
     pass
-"""
+
 
 class MainWidget(Widget):
     pass
 
+class BoxLayoutExample(BoxLayout):
+    pass
+
 
 class FilterApp(App):
-    """
+
     def build(self):
         # Create the screen manager
         sm = ScreenManager()
@@ -39,7 +46,7 @@ class FilterApp(App):
         sm.add_widget(PPGScreen(name='PPG'))
         sm.add_widget(EDAScreen(name='EDA'))
         return sm
-    """
+
     pass
 
 FilterApp().run()
