76af19cca9aa03bb0f331dce73a1bdec727eafb1
Ellen <ellen.stabel@student.kuleuven.be>
2021-11-23 16:12:36 +0100

    16

--- App/App_functies.py
+++ App/App_functies.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -7,16 +8,17 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.core.window import Window
 import matplotlib.pyplot as plt
-# from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+import numpy as np
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
-"""
+
 x = [1, 2, 3, 4, 5]
 y = [5, 12, 6, 9, 15]
 
 plt.plot(x, y)
 plt.xlabel("x-as")
 plt.ylabel("y-as")
-"""
+
 
 class TitleScreen(Screen):
     pass
@@ -27,13 +29,18 @@ class MainScreen(Screen):
 
 
 class ECGScreen(Screen):
-    """
+    pass
+
+
+class AnchorLayoutECG(AnchorLayout):
     def __init__(self, **kwargs):
-        super(ECGScreen, self).__init__(**kwargs)
+        super(AnchorLayoutECG, self).__init__(**kwargs)
         grafiek_ecg = self.ids.grafiek_ECG
         grafiek_ecg.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
     """
     pass
+    """
 
 
 class PPGScreen(Screen):
@@ -56,10 +63,8 @@ bestand = Builder.load_file("filter.kv")
 
 
 class FilterApp(App):
-
     def build(self):
         return bestand
-
 
 
 FilterApp().run()
